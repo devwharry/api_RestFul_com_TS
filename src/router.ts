ec3562542e20f51d6dda01c898426f7105822288
wharry <devwharry@gmail.com>
2024-01-17 10:07:06 -0300

    criação do controller

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,8 @@
 import { Router, Request, Response } from "express";
+import { createMovie } from "./controllers/movieControllers";
 
 const router = Router();
 
 export default router.get("/test", (req: Request, res: Response) => {
     res.status(200).send("PAI working!");
-});
+}).post("/movie", createMovie);
