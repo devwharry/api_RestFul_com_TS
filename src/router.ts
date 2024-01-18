6534222130ad267f22b3fb31ad7acff5733c0dc7
wharry <devwharry@gmail.com>
2024-01-18 10:35:36 -0300

    Validação de filmes, Resgatando filmes por id e resgatando todos os filmes do DB

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,14 @@
 import { Router, Request, Response } from "express";
-import { createMovie } from "./controllers/movieControllers";
+import { createMovie, findMovieById, getAllMovies } from "./controllers/movieControllers";
 
 import { validate } from "./middleware/handleValidation";
+import { movieCreateValidation } from "./middleware/movieValidation";
 
 const router = Router();
 
 export default router.get("/test", (req: Request, res: Response) => {
     res.status(200).send("PAI working!");
-}).post("/movie", validate, createMovie);
+})
+.post("/movie", movieCreateValidation(), validate, createMovie)
+.get("/movie/:id", findMovieById)
+.get("/movie", getAllMovies);
