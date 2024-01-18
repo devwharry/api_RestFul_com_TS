e6f30a922e4dec47e469e1ca2a616f9312587620
wharry <devwharry@gmail.com>
2024-01-18 09:40:03 -0300

    Middleware de validação de erros

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { Router, Request, Response } from "express";
 import { createMovie } from "./controllers/movieControllers";
 
+import { validate } from "./middleware/handleValidation";
+
 const router = Router();
 
 export default router.get("/test", (req: Request, res: Response) => {
     res.status(200).send("PAI working!");
-}).post("/movie", createMovie);
+}).post("/movie", validate, createMovie);
